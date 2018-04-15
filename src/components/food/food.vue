3ccef0f7699a721de34dcb543656e65fb51b713f
zhaolingxue <18222478537@163.com>
2018-04-15 21:17:28 +0800

    ''

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="fade">
-    <div class="food" v-show="showFlag">
+    <div class="food" v-show="showFlag" ref="food">
       <div class="food-content">
         <div class="image-header">
           <img :src="food.image">
@@ -17,6 +17,10 @@
           <div class="price">
             <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">{{food.oldPrice}}</span>
           </div>
+          <div class="cartcontrol-wrapper">
+             <cartcontrol :food="food"></cartcontrol>
+          </div>
+          <div class="buy" v-show="!food.count || food.count === 0">加入购物车</div>
         </div>
       </div>
     </div>
@@ -24,6 +28,8 @@
   
 </template>
 <script type="text/ecmascript-6">
+import BScroll from 'better-scroll'
+import cartcontrol from 'components/cartcontrol/cartcontrol'
   export default {
     name: 'food',
     props: {
@@ -39,10 +45,22 @@
     methods: {
       show() {
         this.showFlag = true
+        this.$nextTick(() => {
+          if(!this.scroll) {
+            this.scroll = new BScroll(this.$refs.food, {
+              click: true
+            })
+          } else {
+            this.scroll.refresh()
+          }
+        })
       },
       hide() {
         this.showFlag = false
       }
+    },
+    components: {
+      cartcontrol
     }
   }
 </script>
@@ -111,6 +129,23 @@
         text-decoration:line-through
         font-size: 10px 
         color: rgb(147,153,159)
+    .cartcontrol-wrapper 
+      position: absolute 
+      right: 12px 
+      bottom: 12px 
+    .buy
+       position: absolute 
+       right: 18px 
+       bottom: 18px 
+       z-index: 10
+       height: 24px 
+       line-height: 24px 
+       padding: 0 12px 
+       box-sizing: border-box 
+       border-radius: 12px 
+       font-size: 10px 
+       color: #fff 
+       background: rgb(0, 160, 220)
      
            
 
