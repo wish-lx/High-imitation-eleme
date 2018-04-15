b61b19f7705cb6484ba71b191c385dc57eea6e1a
zhaolingxue <18222478537@163.com>
2018-04-15 19:11:37 +0800

    ''

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,7 +1,28 @@
 <template>
-  <div class="food" v-show="showFlag">
-    
-  </div>
+  <transition name="fade">
+    <div class="food" v-show="showFlag">
+      <div class="food-content">
+        <div class="image-header">
+          <img :src="food.image">
+          <div class="back" @click="hide">
+            <i class="icon-arrow_lift"></i>
+          </div> 
+        </div>
+        <div class="content">
+          <h1 class="title"> {{food.name}}</h1>
+          <div class="detail">
+            <div class="sell-count">月售{{food.sellCount}}份</div>
+            <div class="rating">好评率 {{food.rating}}%</div>
+          </div>
+          <div class="price">
+            <span class="now">￥{{food.price}}</span>
+            <span class="old" v-show="food.oldPrice">{{food.oldPrice}}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+  
 </template>
 <script type="text/ecmascript-6">
   export default {
@@ -19,6 +40,9 @@
     methods: {
       show() {
         this.showFlag = true
+      },
+      hide() {
+        this.showFlag = false
       }
     }
   }
@@ -32,4 +56,32 @@
   z-index: 30
   width: 100% 
   background: #fff
+  transform: translate3d(0, 0, 0)
+  &.move-enter-active,&.move-leave-active
+    transition: all 0.6s linear 
+  &.move-enter, &.move-leave-active
+    transform: translate3d(100%, 0, 0)
+  .image-header
+    position: relative 
+    width: 100% 
+    height: 0
+    padding-top: 100%
+    img
+     position: absolute 
+     top: 0 
+     left: 0   
+     width: 100% 
+     height: 100%
+    .back
+      position: absolute 
+      top:10px 
+      left: 0 
+      .icon-arrow_lift
+        display: block  
+        padding: 10px
+        font-size: 20px 
+        color: #fff 
+
+       
+
 </style>
