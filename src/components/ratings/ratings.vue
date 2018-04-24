83fa75dd392f01675d7667a2afad4fe096f59777
zhaolingxue <18222478537@163.com>
2018-04-24 11:29:11 +0800

    ''

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -29,7 +29,7 @@
                     :ratings="ratings"></ratingselect>
       <div class="rating-wrapper">
         <ul>
-          <li v-for="rating in ratings" v-show="needShow(rating.rateType, rating.text)" class="rating-item">
+          <li v-for="rating in ratings" :key="rating.id" v-show="needShow(rating.rateType, rating.text)" class="rating-item">
             <div class="avatar">
               <img width="28" height="28" :src="rating.avatar">
             </div>
@@ -42,7 +42,7 @@
               <p class="text">{{rating.text}}</p>
               <div class="recommend" v-show="rating.recommend && rating.recommend.length">
                 <span class="icon-thumb_up"></span>
-                <span class="item" v-for="item in rating.recommend">{{item}}</span>
+                <span class="item" v-for="item in rating.recommend" :key="item.id">{{item}}</span>
               </div>
               <div class="time">
                 {{rating.rateTime | formatDate}}
@@ -77,8 +77,7 @@
         onlyContent: true
       }
     },
-    created() {
-     
+    created () {
       this.$http.get('/api/ratings').then((response) => {
         response = response.body
         if (response.errno === ERR_OK) {
@@ -92,7 +91,7 @@
       })
     },
     methods: {
-      needShow(type, text) {
+      needShow (type, text) {
         if (this.onlyContent && !text) {
           return false
         }
@@ -102,17 +101,17 @@
           return type === this.selectType
         }
       },
-      selectRating(type) {
+      selectRating (type) {
         this.selectType = type
         this.$nextTick(() => {
           this.scroll.refresh()
-        });
+        })
       },
-      toggleContent() {
+      toggleContent () {
         this.onlyContent = !this.onlyContent
         this.$nextTick(() => {
           this.scroll.refresh()
-        });
+        })
       }
     },
     filters: {
@@ -126,7 +125,7 @@
       split,
       ratingselect
     }
-  };
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,12 +1,80 @@
 <template>
-  <div id="seller">
-    seller
+  <div class="seller">
+    <div class="seller-content ">
+      <div class="overview">
+        <h1 class="title">{{seller.name}}</h1>
+        <div class="desc border-1px">
+          <star :size="36" :score="seller.score"></star>
+          <span class="text">{{seller.ratingCount}}</span>
+          <span class="text">{{seller.sellCount}}</span>
+        </div>
+        <ul class="remark">
+          <li class="block">
+            <h2>起送价</h2>
+            <div class="content">
+              <span class="stress">{{seller.minPrice}}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>商家配送</h2>
+            <div class="content">
+              <span class="stress">{{seller.deliveryPrice}}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>平均配送时间</h2>
+            <div class="content">
+              <span class="stress">{{seller.minPrice}}</span>分钟
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 <script type="text/ecmascript-6">
+import star from 'components/star/star'
 export default {
-  name: 'seller'
+  name: 'seller',
+  props:{
+    seller:Object
+  },
+  components: {
+    star
+  }
 }
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
+@import '../../common/stylus/mixin.styl'
+.seller 
+  position: absolute 
+  top: 174px 
+  bottom: 0 
+  left: 0 
+  width: 100% 
+  overflow: hidden 
+  .overview 
+    padding: 18px 
+    .title
+     margin-bottom: 8px 
+     line-height: 14px 
+     color: rgb(7,17,27)
+     font-size: 14px
+     .desc 
+       padding-bottom: 18px
+       line-height: 18px 
+       border-1px(rgba(7,17,27,0.1))
+       font-size: 0
+       .star 
+         display: inline-block 
+         margin-right: 8px 
+         vertical-align: top
+       .text 
+         margin-right: 12px 
+         display: inline-block 
+         vertical-align: top
+         font-size: 10px 
+         color: rgb(77,85,93)
+
+
 </style>
